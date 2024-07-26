71b66fd7b2a34afa113219ca1cfe9e57662f1834
amanraghuvanshi <iamanraghuvanshi@gmail.com>
2024-07-26 12:54:47 +0530

    updation in requirements

---
+++ app.py
@@ -0,0 +1,14 @@
+# import os, pathlib, streamlit, google.generativeai
+import os
+import pathlib
+import streamlit as st
+import google.generativeai as genai
+
+# setting up the page configuration
+st.set_page_config(
+    page_title="Life Medical AI Assistant", page_icon="🧊", layout="centered", initial_sidebar_state="expanded"
+)
+
+# setting up the logo
+logo_path = pathlib.Path(__file__).parent / "assets" / "logo.png"
+st.image(logo_path, use_column_width=True)
