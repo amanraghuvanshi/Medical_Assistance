f5a36da144cdef088949af8e2d938f69eda49243
amanraghuvanshi <iamanraghuvanshi@gmail.com>
2024-07-30 11:33:30 +0530

    updations:   app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import google.generativeai as genai
 
 # setting up the page configuration
 st.set_page_config(
-    page_title="Life Medical AI Assistant", page_icon="🧊", layout="centered", initial_sidebar_state="expanded"
+    page_title="Life Medical AI Assistant", page_icon=":robot", layout="centered", initial_sidebar_state="expanded"
 )
 
 # setting up the logo
-logo_path = pathlib.Path(__file__).parent / "assets" / "logo.png"
+logo_path = pathlib.Path(__file__).parent
 st.image(logo_path, use_column_width=True)
