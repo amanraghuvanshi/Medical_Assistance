375ee082a99ae291780202714a82dc82320cfe1d
amanraghuvanshi <iamanraghuvanshi@gmail.com>
2024-07-30 12:00:21 +0530

    updations

--- app.py
+++ app.py
@@ -9,5 +9,14 @@ st.set_page_config(
     page_title="Life Medical AI Assistant", page_icon=":robot", layout="centered", initial_sidebar_state="expanded"
 )
 
-logo_path = pathlib.Path("assets/logo.png")
-st.image(logo_path, use_column_width=True)
+logo_path = pathlib.Path("./logo.jpg")
+# create the image in the sidebar
+# st.sidebar.image(logo_path, use_column_width=True)
+
+st.title("Life Medical AI Assistant 🤖")
+
+st.subheader("Welcome to the new revolutionizing solution to Life🏥")
+
+uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+submit = st.button("Generate Report 📄")
